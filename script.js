80250781b9973fd92d7786be5118d8ebee2fe9e7
Sravan S Nair <84452323+livelysravan@users.noreply.github.com>
2024-10-19 14:42:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function greet(name) {
+    console.log('Hello, ${name}!');
+    }
+greet("Sonu")
+
+const greet2 = function(name) {
+    console.log('Hello', name);
+    };
+greet2("prakash")
+
+const greet3 = (name)=>{
+    console.log('Hello',name);
+};
+greet3("prakash")
+
+
+var str = "Hello World";
+var num = 123;
+var bool = true;
+var nullVar = null;
+var undefVar = undefined;
+var BigInt = 7823647237467823647823;
+var sym = Symbol("Hello World");
+var obj = {name:"John", age:30};
+var arr = [1,2,3,4,5];
+
+console.log(str);
+console.log(num);
+console.log(bool);
+console.log(nullVar);
+console.log(undefVar);
+console.log(BigInt);
+console.log(sym);
+console.log(obj);
+console.log(arr);
+
+
+
+
+
+
+
+
